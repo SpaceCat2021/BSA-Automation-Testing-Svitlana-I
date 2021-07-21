a1ce82753313a4440769750b24ae5a4b142b64bd
SpaceCat2021 <83554687+SpaceCat2021@users.noreply.github.com>
2021-07-21 15:21:09 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,16 @@
 const { AuthPage } = require('./auth.page');
+const { SinglPage } = require('./singl.page');
+const { EditPage } = require('./edit.page');
+const { ProfilePage } = require('./profile.page');
+const { AddPage } = require('./add.page');
 
 class App {
   constructor() {
     this.authPage = new AuthPage();
+    this.singlPage = new SinglPage();
+    this.editPage = new EditPage();
+    this.profilePage = new ProfilePage();
+    this.addPage = new AddPage();
   }
 }
 
